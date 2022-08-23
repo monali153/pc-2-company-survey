50e583fbaee16f6bb528c2d43775f42a05129195
Monali Sharma <mona.chaudhari444@gmail.com>
2022-08-23 18:10:53 +0530

    complete

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -1,22 +1,36 @@
 package com.jap;
 
+import java.util.Scanner;
+
 public class AverageAge {
 
-    public String averageAgeCalculator(String[] ageRawData)
-    {
-        double avg = 0;
+    public static void main(String[] args) {
 
-        try{
-            //Write the logic
+        Scanner scanner = new Scanner(System.in);
 
-            //Handle specific exception
-        }catch (Exception exception)
-        {
-            return exception.toString();
+        System.out.println("Enter no of employee");
+        String[] age = new String[scanner.nextInt()];
+        System.out.println("Enter age of employee");
+
+        for (int i = 0; i < age.length; i++) {
+            age[i] = String.valueOf(scanner.nextInt());
         }
-        return String.valueOf(avg);
+        AverageAge averageAge = new AverageAge();
+        System.out.println(averageAge.averageAgeCalculator(age));
+
     }
 
-
+    public String averageAgeCalculator(String[] ageRawData) {
+        double average = 0;
+        try {
+            for (int i = 0; i < ageRawData.length; i++) {
+                average = average + Double.parseDouble(ageRawData[i]);
+            }
+            average = average / ageRawData.length;
+        } catch (NumberFormatException e) {
+            return e.toString();
+        }
+        return String.valueOf(average);
+    }
 
 }
